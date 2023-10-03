69163ca31dd14dd30ced59cd14e3ed4e8d3e9ea1
Peter Dumbari <dummytwiz@gmail.com>
2023-10-03 13:31:37 +0100

    added redux and axios

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Greeting from "./Greeting";
+import store from "../../store";
+import { Provider } from "react-redux";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Greeting />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Greeting />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 };
 
